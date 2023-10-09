f2742011be0f5fa5e71c8aa763b6b30a5b3be527
liangyi <994302767@qq.com>
2023-10-09 15:41:41 -0500

    Update glados.py

--- glados.py
+++ glados.py
@@ -29,8 +29,8 @@ if __name__ == '__main__':
         email = state.json()['data']['email']
         if 'message' in checkin.text:
             mess = checkin.json()['message']
-            print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
-            sendContent += email+'----'+mess+'----剩余 '+time+' )天\n'
+            print(email+'----结果--'+mess+'----剩余('+ str(time)+')天')  # 日志输出
+            sendContent += email+'----'+mess+'----剩余 '+str(time)+' )天\n'
         else:
             requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
             print('cookie已失效')  # 日志输出
